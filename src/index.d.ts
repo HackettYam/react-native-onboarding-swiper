4a4dbcd208697052b71f0389704c3e1851d25b0d
HackettYam <yam.music@hotmail.com>
2019-03-27 01:54:49 -0500

    Fix support typescript

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-declare class Onboarding extends React.Component {
+declare class Onboarding extends Component {
 }
 
 declare module 'react-native-onboarding-swiper'
